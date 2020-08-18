712db8f591e9f4d6a00a712c265ddd19fc07d9d4
John Brunton <john_brunton@live.co.uk>
2020-08-18 19:10:59 +0100

    call through to node services, add override volumes

--- services/api/run.rb
+++ services/api/run.rb
@@ -1,9 +1,27 @@
 require 'sinatra'
 require "sinatra/json"
+require "sinatra/reloader" if development?
+require 'httparty'
 
 set :port, 5000
 set :bind, '0.0.0.0'
 
 get '/' do
   json greeting: 'Hello world!'
+end
+
+def get_audience
+  response = HTTParty.get('http://audience-service:3000')
+  JSON.parse(response.body)['audience']
+end
+
+def get_greeting
+  response = HTTParty.get('http://greeting-service:3000')
+  JSON.parse(response.body)['greeting']
+end
+
+get '/test' do
+  audience = get_audience
+  greeting = get_greeting
+  json audience: audience, template: greeting, greeting: greeting % audience
 end
